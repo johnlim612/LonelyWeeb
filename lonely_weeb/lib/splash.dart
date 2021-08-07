c1f55f6c2b55f60fad6e0543a8b9ffdfa0fb19a3
johnlim612 <johnlim0612@gmail.com>
2021-08-06 20:56:59 -0700

    Splash Screen

--- lonely_weeb/lib/splash.dart
+++ lonely_weeb/lib/splash.dart
@@ -1,14 +1,55 @@
+import 'main.dart';
 import 'package:flutter/material.dart';
+import 'package:lonely_weeb/main.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: 'Lonley Weeb',
+                )));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('Splash Screen',
-            style: TextStyle(fontSize: 42, fontFamily: "RussoOne")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text('Lonely Weeb',
+                  style: TextStyle(fontSize: 42, fontFamily: "RussoOne")),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 40),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/chika_fujiwara.jpg'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.circle),
+              height: 236,
+              width: 236,
+            ),
+          ],
+        ),
       ),
     );
   }
